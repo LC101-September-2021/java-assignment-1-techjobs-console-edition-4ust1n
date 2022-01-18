61c3488ecbfb44b36eb6a8cea84e0127a78fc4c9
Austin <austinjdiehl@gmail.com>
2022-01-17 21:47:18 -0600

    Updated Files.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No results found.");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n*****");
+
+                for (Map.Entry<String, String> jobResult : job.entrySet()) {
+                    System.out.println(jobResult.getKey() + ": " + jobResult.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
